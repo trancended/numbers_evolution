79fe44faa66a426a8fcf89960c72d770beb8f939
Dariusz Lubiński <trancended@gmail.com>
2025-11-26 19:40:47 +0100

    Fix IP binding and add PHX_SERVER for Fly.io

--- config/runtime.exs
+++ config/runtime.exs
@@ -69,11 +69,8 @@ if config_env() == :prod do
   config :numbers_evolution, NumbersEvolutionWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      # Bind on all interfaces (IPv4) for Fly.io
+      ip: {0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
